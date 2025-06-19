9d9ef1740c5936f4f53a52d05ebf7b213978d483
Derek Jass <jazzer36@gmail.com>
2025-06-19 14:48:28 -0500

    Custom lookbacks.

--- sts_run_parser/sts_run_parser.py
+++ sts_run_parser/sts_run_parser.py
@@ -18,6 +18,14 @@ class State(rx.State):
         rundata = sorted(list(filter(run_filter, runs())), key=sort_key)
         return average_floor_data(rundata, self.lookback, self.character)
 
+    @rx.event
+    def change_character(self, value: str):
+        self.character = value
+
+    @rx.event
+    def change_lookback(self, value: list[int | float]):
+        self.lookback = value[0]
+
 
 def index() -> rx.Component:
     # Welcome Page (Index)
@@ -25,13 +33,42 @@ def index() -> rx.Component:
         rx.color_mode.button(position="top-right"),
         rx.vstack(
             rx.recharts.line_chart(
-                rx.recharts.line(data_key='winrate'),
-                rx.recharts.line(data_key='avg_floor'),
+                rx.recharts.line(
+                    data_key='winrate',
+                    stroke='#82ca9d',
+                    dot=False,
+                    type_='monotone',
+                    stroke_width=2),
+                rx.recharts.line(
+                    data_key='avg_floor',
+                    stroke='#8884d8',
+                    dot=False,
+                    type_='monotone',
+                    stroke_width=2),
                 rx.recharts.x_axis(data_key='run'),
                 rx.recharts.y_axis(),
+                rx.recharts.graphing_tooltip(),
+                rx.recharts.legend(),
                 data=State.data,
                 width="100%",
-                height=300
+                height=500
+            ),
+            rx.hstack(
+                rx.select(
+                    ['ALL', 'IRONCLAD', 'THE_SILENT', 'DEFECT', 'WATCHER'],
+                    value=State.character,
+                    on_change=State.change_character
+                ),
+                rx.vstack(
+                    rx.heading(State.lookback),
+                    rx.slider(
+                        on_value_commit=State.change_lookback,
+                        min_=1,
+                        max=500
+                    ),
+                    width='100%'
+                ),
+                width='100%'
             )
         ),
         rx.logo(),
